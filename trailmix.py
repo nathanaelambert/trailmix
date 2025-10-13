01cce64e46e5029af2ec85b46b16f9d2051b4c89
Nathanael Lambert <nathanael.lambert@protonmail.com>
2025-10-13 11:48:55 +0200

    basic app

---
+++ trailmix.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.write("""
+# Trailmix
+*Meet your personal AI meal planner*
+""")
+
+### Onboarding questionnaire
+
+# number of meals every week
+# body weight
+# activity level
+# goals (loose weight, build muscle, reduce meat consumption)
+# dietary restrictions, & alergies
+# diet (vegetarian, keto...)
+# location (maybe later)
