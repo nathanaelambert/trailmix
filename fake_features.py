79dacc9b6ffb569d2a3b17e1e381749e1a3f6ce2
Nathanael Lambert <nathanael.lambert@protonmail.com>
2025-10-29 14:20:56 +0100

    google analytics inject

--- fake_features.py
+++ fake_features.py
@@ -1,11 +1,46 @@
 import streamlit as st
-## onboarding questionnaire
+import pathlib
+from bs4 import BeautifulSoup
+import logging
+import shutil
 
-## recipe generation button 
-print("fake recipe:\n- ingredient 1 (200g)\n- ingedient 2(100g)")
-# display "order"
 
-import streamlit as st
+def inject_google_analytics():
+    GA_stream_id = 12364028308
+    GA_measurement_id = "G-ETT9HS0JXE"
+    GA_JS = """
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-ETT9HS0JXE"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-ETT9HS0JXE');
+    </script>
+    """
+
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_measurement_id):
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)
+        else:
+            shutil.copy(index_path, bck_index)
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+
+
+
+
+
+
+
+
+
+
+
+
 
 st.title("TRAILMIX")
 st.write("##insert onboarding questionnaire")
